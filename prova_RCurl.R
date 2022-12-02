d4d45f0fe3d388275124891de1c3ff6568e97664
federico00taddei <icotaddei@gmail.com>
2022-12-02 11:26:13 +0100

    I adjust the code try to see it and then we marge

--- prova_RCurl.R
+++ prova_RCurl.R
@@ -3,21 +3,10 @@
 library(RCurl)
 
 url <- "https://beppegrillo.it/un-mare-di-plastica-ci-sommergera/"
-email <- "chiaramoruzzi@gmail.com"
+email <- "icotaddei@gmail.com"
 agent <- R.Version()$version.string
 user_id <- c(From = email, `User-Agent` = agent)
   
-web_page <- RCurl::getURL(url, httpHEAD(user_id)
+web_page <- RCurl::getURL(url=url, httpheader= (user_id))
 
-
-
-
-
-
-
-
-
-
-
-
-
+#it wasn't working like you write before, now I put url=... and httpheader= userid and closed with ")"
