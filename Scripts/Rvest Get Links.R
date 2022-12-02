bb13ba32dc5debbc0afbbc88c8372a9ede247c87
federico00taddei <icotaddei@gmail.com>
2022-12-02 12:09:51 +0100

    Get all links coding and put in data frame

---
+++ Scripts/Rvest Get Links.R
@@ -0,0 +1,12 @@
+#Get all links CODING
+
+install.packages("XML")
+
+#To get all the link in the HTML that we download
+url
+all_link = XML::getHTMLLinks(web_page, externalOnly = T)
+
+all_link
+
+#Now we makle a data frame to collect all the links
+df_links = tibble(all_link)
